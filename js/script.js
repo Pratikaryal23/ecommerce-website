16faa934b3e001fa9325ef126c99261f4e65d2ae
pratik aryal <aryalpratik057@gmail.com>
2024-01-22 23:52:38 +0545

    third commit

--- js/script.js
+++ js/script.js
@@ -1,4 +1,25 @@
-// addEventListener("scroll",()=>{
-//     console.log(window.scrollY)
 
-// })
+let solo=document.getElementById('solo');
+
+
+addEventListener("scroll",()=>{
+    console.log(window.scrollY)
+    if(window.scrollY>10){
+        solo.style.position='sticky'
+        solo.style.background='white'
+    }else{
+        solo.style.position='relative'
+        solo.style.background='transparent'
+    }
+
+})
+let sidebar=document.getElementById('sidebar');
+function Showsdara (){
+    if(sidebar.style.transform==='translateY(0%)'){
+sidebar.style.transform='translateY(-150%)'
+
+    }else{
+sidebar.style.transform='translateY(0%)'
+
+    }
+}
